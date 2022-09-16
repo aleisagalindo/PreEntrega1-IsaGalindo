21672de29d8b943500be94b875fcded24776ac58
Alejandro Isa Galindo <alejandroisagalindo@gmail.com>
2022-09-16 20:40:32 -0300

    Anñadi las cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from './components/Navbar';
+import MainSection from "./components/MainSection";
 import "./App.css"
 
 const App = () => {
   return (
     <>
       <Navbar/>
+      <MainSection/>
     </>
   );
 };
